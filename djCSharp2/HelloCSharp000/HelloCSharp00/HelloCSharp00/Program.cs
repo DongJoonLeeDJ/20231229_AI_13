6249f9f0212451ed38622f1773e964831ddc336a
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-05-27 16:34:10 +0900

    upload csharp

---
+++ djCSharp2/HelloCSharp000/HelloCSharp00/HelloCSharp00/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.out.println = 출력
+            Console.WriteLine("출력 후 한 줄 띄기");
+            Console.Write("출력 후 한 줄 띄지 않음"); //System.out.print랑 동일
+            Console.WriteLine("cw 탭탭");
+            Console.WriteLine(); //java에서는 sysout 적고 ctrl space를 했음
+           
+
+            //문자열 처리
+            //java에서는 get을 썼었음
+            Console.WriteLine("안녕하세요"[0]); //안 이라는 글자 출력
+
+            //기본적으로 덧셈 연산은 자바랑 유사함
+            //덧셈을 이용해서 문자열 더하기 되고, 숫자 연산도 됨
+
+            string s = "Hello"; //java에서는 무조건 String으로 썼음
+            bool b = true; //java에서는 Boolean을 썼음
+
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine("안녕");
+            }
+
+            //foreach문이 java랑 다름
+            //java는 foreach문 개념만 있음, for(int item : nums)
+            //c#은 foreach라는 문법이 있음
+            int[] nums = new int[] { 3, 7, 5, 9, 2 };
+            foreach(int item in nums)
+            {
+                Console.WriteLine(  item );
+            }
+
+            //java에 있는 삼항연산자, if, if else, switch다 있음
+            //for뿐 아니라 while, do while도 있음
+            //Scanner s = new Scanner(System.in); = 입력
+            Console.WriteLine("이름은 무엇인가요?");
+            string name = Console.ReadLine();
+            Console.WriteLine("나이는?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("나이는 " + age + "살입니다." );
+
+            introduce("이동준", 35);
+            introduce("이지환", 15);
+
+
+            reverseSign(nums);
+
+            foreach(int item in nums)
+                Console.WriteLine(item);
+
+
+        }
+
+        //배열을 매개변수로 한 뒤, 함수에서 변경하면 메인에서도 변경됨
+        //이는 C,Java에서도 해당되는 특징
+        //배열은 포인터니까...(java, c#에선 배열을 클래스로 봄)
+        //클래스 역시 일종의 포인터에서 유래된 자료형
+        static void reverseSign(int[] nums)
+        {
+            for(int i = 0; i<nums.Length; i++)
+                nums[i] *= -1;
+        }
+
+        static void introduce(string name, int age)
+        {
+            Console.WriteLine("이름 : " + name);
+            Console.WriteLine("나이 : " + age);
+            if(age<20)
+            {
+                Console.WriteLine("학교를 다니고 있을 가능성이 높아요");
+            }
+            else
+            {
+                Console.WriteLine("회사를 다니고 있을 가능성이 높아요.");
+            }
+        }
+    }
+}
---
+++ djCSharp2/HelloCSharp000/HelloCSharp00/HelloCSharp01/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrnums = new int[5];
+            arrnums[0] = int.Parse(Console.ReadLine());
+            bool result = int.TryParse(Console.ReadLine(), out arrnums[1]);
+
+            if (!result)
+                Console.WriteLine("두 번째 값은 잘못된 값으로 보여요");
+
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                arrnums[2] = number;
+            }
+            catch (Exception ex) //이벤트 때문에 주로 ex대신 e를 씀
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                arrnums[2] = 0;
+            }
+
+            for (int i = 3; i < arrnums.Length; i++)
+                arrnums[i] = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < arrnums.Length; i++)
+            {
+                if (arrnums[i] < 0)
+                    arrnums[i] *= -1; //음수가 있을 경우 -1을 곱하여 양수로 만든다.
+            }
+
+            int max = arrnums[0];
+            for (int i = 1; i < arrnums.Length; i++)
+            {
+                if (max >= arrnums[i])
+                    continue;
+                max = arrnums[i];
+            }
+            Console.WriteLine("최댓값 : " + max);
+
+            
+            clearArr(arrnums); //배열 값을 0으로 초기화할 함수
+            inputArr(arrnums); //배열에 값을 넣을 것
+            reverseSign(arrnums); //배열의 값들에 대한 부호 정리(음수는 양수로)
+            //int aa = 100;
+            max = findMaxNum(arrnums); //최댓값 찾아서 리턴
+
+            Console.WriteLine($"최댓값은 {max}입니다.");
+
+
+            inputArr(arrnums, new Random());
+            max = findMaxNum(arrnums);
+            Console.WriteLine(string.Format("최댓값은 {0}입니다.", max));
+        }
+
+        
+
+        private static int findMaxNum( int[] nums)
+        {
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (max < nums[i])
+                    max = nums[i];
+            }
+            return max;
+        }
+
+        private static void reverseSign(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                    nums[i] *= -1;
+            }
+        }
+
+        //오버로딩 : 매개변수 다르면 함수 이름이나 생성자 이름 똑같아도 
+        //서로 다른 걸로 친다....(C에 없고 java에 있는 개념)
+        private static void inputArr(int[] nums, Random random)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = random.Next(100); //0~99
+            }
+        }
+        private static void inputArr(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                try
+                {
+                    nums[i] = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("입력 중단!");
+                    return;
+                }
+                finally
+                {
+                    Console.WriteLine("대신 이건 무조건 출력하게 되어 있다!!! 기억나지?");
+                }
+            }
+        }
+
+        private static void clearArr(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = 0;
+        }
+    }
+}
