4082a1f1adeac1624e87ba457444c1d882b597da
DongJoonLeeDJ <ldjgood12@gmail.com>
2024-05-27 16:42:42 +0900

    드디어 ignore 성공

--- djCSharp2/HelloCSharp000/HelloCSharp00/HelloCSharp01/Program.cs
+++ djCSharp2/HelloCSharp000/HelloCSharp00/HelloCSharp01/Program.cs
@@ -11,9 +11,9 @@ namespace HelloCSharp01
         static void Main(string[] args)
         {
             int[] arrnums = new int[5];
-            Console.WriteLine("첫번째 값 입력");
+            Console.WriteLine("첫번째 값 입력!");
             arrnums[0] = int.Parse(Console.ReadLine());
-            Console.WriteLine("두번째 값 입력");
+            Console.WriteLine("두번째 값 입력!");
             bool result = int.TryParse(Console.ReadLine(), out arrnums[1]);
 
             if (!result)
